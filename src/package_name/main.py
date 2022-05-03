ffba4e3d73015eef74561d09031b2b854feeaf5d
Lucas Viana <lucas.viana@volvocars.com>
2022-05-03 14:32:47 +0200

    Add first version of project template
    Create Python project template with packaging and testing support.
    
    Packaging:
    With `setuptools` using `pyproject.toml` in and `setup.cfg`.
    Transitioning to a project fully on `pyproject.toml` would require
    either switching to Poetry or wait for `setuptools` to addapt.
    
    Testing:
    With Pytest, MyPy, Tox, coverage and generation of HTML reports.
    Coverage is currently buggy but I have spent too much time in this for
    the moment.

---
+++ src/package_name/main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main() -> int:
+    """Entry point."""
+    print(f'Program started with {sys.argv}')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ test/unit/test_main.py
@@ -0,0 +1,6 @@
+from package_name import main
+
+
+def test_main():
+    """A long line."""
+    assert main.main() == 0
